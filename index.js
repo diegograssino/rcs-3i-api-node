297989534dc01bad18e9a65ea80d5a97e944132b
Diego Grassino <diegograssino@gmail.com>
2022-10-27 23:13:19 -0300

    Fin de la clase

--- index.js
+++ index.js
@@ -1,69 +1,73 @@
+// Configuracion de express
 const express = require('express');
 const app = express();
+// Configuracion del body parser de express
 app.use(express.json());
+// Configuracion de dotenv
 require('dotenv').config();
+// Configuracion de CORS (evito errores de CORS)
+var cors = require('cors');
+app.use(cors());
+let corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+// Importo el mock
+const MOCK = require('./mocks/mocks');
 
+// Dejo seteado en una constante el puerto, usar el que este en el env, sino 800
 const PORT = process.env.PORT || 8000;
 
-const MOCK = [
-  {
-    id: '1',
-    title:
-      'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-    price: 109.95,
-    description:
-      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-    category: "men's clothing",
-    image:
-      'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-    rating: {
-      rate: 3.9,
-      count: 120,
-    },
-  },
-  {
-    id: '2',
-    title:
-      'Mens Casual Premium Slim Fit T-Shirts ',
-    price: 22.3,
-    description:
-      'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-    category: "men's clothing",
-    image:
-      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-    rating: {
-      rate: 4.1,
-      count: 259,
-    },
-  },
-];
+// Empiezo a programar los endopoints
+app.get(
+  '/products',
+  cors(corsOptions),
+  (req, res) => {
+    console.log('GET /products');
+    const allProducts = MOCK;
+    res.status(200).send(allProducts);
+  }
+);
 
-app.get('/products', (req, res) => {
-  console.log('GET /products');
-  const allProducts = MOCK;
-  res.status(200).send(allProducts);
-});
+app.get(
+  '/product/:id',
+  cors(corsOptions),
+  (req, res) => {
+    const { id } = req.params;
+    console.log('GET /product/' + id);
+    const product = MOCK.find(p => p.id === id);
 
-app.get('/product/:id', (req, res) => {
-  const { id } = req.params;
-  console.log('GET /product/' + id);
-  const product = MOCK.find(p => p.id === id);
+    res.status(200).json(product);
+  }
+);
 
-  res.status(200).send(product);
-});
+app.post(
+  '/checkout',
+  cors(corsOptions),
+  (req, res) => {
+    const { body } = req;
+    console.log('POST /checkout/');
+    console.log(body);
+    body.id = '1';
 
+    res.status(200).json(body);
+  }
+);
+
+// Ejemplo utilizando el router de express
 app
   .route('/test')
   .get((req, res) => {
     console.log('GET /products');
     const allProducts = MOCK;
-    res.status(200).send(allProducts);
+    res.status(200).json(allProducts);
   })
   .post((req, res) => {
     console.log(req.body);
-    res.send('👍');
+    res.json('👍');
   });
 
+// Esta funcion es la que corre la API, si no esta, no se autoejecuta.
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
 });
